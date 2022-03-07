ea3ed5dcfc5707a039f017bc44fcbd13d8ec532f
HaruKishima <18120256@student.hcmus.edu.vn>
2022-03-07 13:35:34 +0700

    fix swagger

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,10 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 const setup = (app: INestApplication) => {
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
@@ -22,10 +26,6 @@ const setup = (app: INestApplication) => {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('v1/docs', app, document);
-  app.enableVersioning({
-    type: VersioningType.URI,
-    defaultVersion: '1',
-  });
 };
 
 async function bootstrap() {
