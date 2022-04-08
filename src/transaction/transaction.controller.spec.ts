4d205000883b7a3ecc4a6581908dc11b01cadfe7
harukishima <18120256@student.hcmus.edu.vn>
2022-04-08 14:17:27 +0700

    fix test case

--- src/transaction/transaction.controller.spec.ts
+++ src/transaction/transaction.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TransactionCoinService } from './transaction-coin/transaction-coin.service';
 import { TransactionController } from './transaction.controller';
+
+const mockTransactionService = {};
 
 describe('TransactionController', () => {
   let controller: TransactionController;
@@ -7,6 +10,12 @@ describe('TransactionController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TransactionController],
+      providers: [
+        {
+          provide: TransactionCoinService,
+          useValue: mockTransactionService,
+        },
+      ],
     }).compile();
 
     controller = module.get<TransactionController>(TransactionController);
